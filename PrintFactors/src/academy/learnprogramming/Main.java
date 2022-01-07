b064d7fb3db90199ebe11fb7c8ea61b4bba75841
jungmoto <71113104+jungmoto@users.noreply.github.com>
2022-01-07 13:05:07 -0500

    Add files via upload

---
+++ PrintFactors/src/academy/learnprogramming/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	printFactors(10);
+    }
+
+    public static void printFactors(int number) {
+        if (number >= 1) {
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+}
