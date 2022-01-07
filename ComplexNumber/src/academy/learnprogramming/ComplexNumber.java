6343db1704de2556ab7e7d103c60dfc4b093af31
jungmoto <71113104+jungmoto@users.noreply.github.com>
2022-01-07 13:56:17 -0500

    Add files via upload

---
+++ ComplexNumber/src/academy/learnprogramming/ComplexNumber.java
@@ -0,0 +1,39 @@
+package academy.learnprogramming;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real = real;
+    }
+
+    public double getImaginary() {
+        return this.imaginary = imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complex) {
+        this.real += complex.getReal();
+        this.imaginary += complex.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complex) {
+        this.real -= complex.getReal();
+        this.imaginary -= complex.getImaginary();
+    }
+}
